877f11e0911dc5f9d8e0e397651a0ebf6fcc5190
di879891 <di879891@ucf.edu>
2026-05-13 20:59:49 -0400

    conflict

--- OrbitalDecay/Source/OrbitalDecay/OrbitalDecayGameMode.cpp
+++ OrbitalDecay/Source/OrbitalDecay/OrbitalDecayGameMode.cpp
@@ -13,3 +13,47 @@ AOrbitalDecayGameMode::AOrbitalDecayGameMode()
 	// This "wires" = custom HUD class to the GameMode
 	HUDClass = AMyHUD::StaticClass();
 }
+
+void AOrbitalDecayGameMode::BeginPlay()
+{
+    Super::BeginPlay();
+    SetGameState(EGameState::Playing);
+}
+
+void AOrbitalDecayGameMode::SetGameState(EGameState NewState)
+{
+    CurrentGameState = NewState;
+
+    switch (CurrentGameState)
+    {
+    case EGameState::Playing:
+        // TODO: re-enable ALanderPawn input
+        // TODO: restart MathGenerator
+        UE_LOG(LogTemp, Warning, TEXT("GameState -> Playing"));
+        break;
+
+    case EGameState::Success:
+        // TODO: disable ALanderPawn input
+        // TODO: show success UI on AMyHUD
+        UE_LOG(LogTemp, Warning, TEXT("GameState -> Success"));
+        StartReplay();
+        break;
+
+    case EGameState::Failure:
+        // TODO: disable ALanderPawn input
+        // TODO: show failure UI on AMyHUD
+        UE_LOG(LogTemp, Warning, TEXT("GameState -> Failure"));
+        StartReplay();
+        break;
+
+    case EGameState::Replay:
+        // TODO: tell ReplayRecorder to play back
+        UE_LOG(LogTemp, Warning, TEXT("GameState -> Replay"));
+        break;
+    }
+}
+
+void AOrbitalDecayGameMode::TriggerSuccess() { SetGameState(EGameState::Success); }
+void AOrbitalDecayGameMode::TriggerFailure() { SetGameState(EGameState::Failure); }
+void AOrbitalDecayGameMode::StartReplay() { SetGameState(EGameState::Replay); }
+void AOrbitalDecayGameMode::RestartGame() { /* Sprint 5 */ }
--- OrbitalDecay/Source/OrbitalDecay/OrbitalDecayGameMode.h
+++ OrbitalDecay/Source/OrbitalDecay/OrbitalDecayGameMode.h
@@ -4,14 +4,37 @@
 #include "GameFramework/GameModeBase.h"
 #include "OrbitalDecayGameMode.generated.h"
 
+UENUM(BlueprintType)
+enum class EGameState : uint8
+{
+    Playing  UMETA(DisplayName = "Playing"),
+    Replay   UMETA(DisplayName = "Replay"),
+    Success  UMETA(DisplayName = "Success"),
+    Failure  UMETA(DisplayName = "Failure")
+};
+
 UCLASS()
 class ORBITALDECAY_API AOrbitalDecayGameMode : public AGameModeBase
 {
-	GENERATED_BODY()
+    GENERATED_BODY()
 
 public:
-	AOrbitalDecayGameMode();
+    AOrbitalDecayGameMode();
 
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Game Progress")
-	int32 GlobalLevel = 1;
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Game Progress")
+    int32 GlobalLevel = 1;
+
+    UPROPERTY(BlueprintReadOnly, Category = "Game State")
+    EGameState CurrentGameState;
+
+    UFUNCTION(BlueprintCallable, Category = "Game State")
+    void SetGameState(EGameState NewState);
+
+    void TriggerSuccess();
+    void TriggerFailure();
+    void StartReplay();
+    void RestartGame(); // stub   Sprint 5
+
+protected:
+    virtual void BeginPlay() override;
 };
